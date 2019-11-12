5660ffa3a4238accbecb9e0398a72f0b543d54d8
shijun <shijun@lixin360.com>
2019-11-12 14:47:55 +0800

    1、解决登录时，键盘弹出造成的页面按钮被遮挡的问题

--- opensource/src/main/java/com/quxianggif/opensource/view/LoginLayout.kt
+++ opensource/src/main/java/com/quxianggif/opensource/view/LoginLayout.kt
@@ -18,6 +18,7 @@
 package com.quxianggif.opensource.view
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
@@ -35,6 +36,62 @@ class LoginLayout(context: Context, attributes: AttributeSet) : LinearLayout(con
 
     var keyboardShowed = false
 
+    var loginInputElementsPreferHeight = -1
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        // 键盘弹出后 loginLayoutBottom 的最小高度为
+        if (loginInputElementsPreferHeight == -1)
+            loginInputElementsPreferHeight = loginInputElements.height
+        if (changed) {
+            val width = right - left
+            val height = bottom - top
+            if (height.toFloat() / width.toFloat() < 4f / 3f) { // 如果高宽比小于4:3说明此时键盘弹出
+                // 按照 1.5 / 4 比例计算 loginLayoutBottom 的高度
+                val loginInputElementsAvailableHeight = (height * (4 / 5.5)).toInt()
+                // 如 loginLayoutBottom 可以承载 loginInputElements 的高度则直接按照比例分配
+                // 反之则优先分配 loginLayoutBottom 高度, 剩余高度设置给 loginLayoutTop
+                if (loginInputElementsAvailableHeight >= loginInputElementsPreferHeight){
+                    post {
+                        loginBgWallLayout.visibility = View.INVISIBLE
+                        processLayoutParams(loginLayoutTop, 0, 1.5f)
+                        processLayoutParams(loginLayoutBottom, 0, 4f)
+                        keyboardShowed = true
+                        requestLayout()
+                    }
+                } else {
+                    post {
+                        loginBgWallLayout.visibility = View.INVISIBLE
+                        processLayoutParams(loginLayoutTop, height - loginInputElementsPreferHeight, 0f)
+                        processLayoutParams(loginLayoutBottom, loginInputElementsPreferHeight, 0f)
+                        keyboardShowed = true
+                        requestLayout()
+                    }
+                }
+            } else {
+                if (keyboardShowed) {
+                    post {
+                        loginBgWallLayout.visibility = View.VISIBLE
+                        processLayoutParams(loginLayoutTop, 0, 6f)
+                        processLayoutParams(loginLayoutBottom, 0, 4f)
+                        requestLayout()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun processLayoutParams(view: View, height: Int, weight: Float){
+        val params = view.layoutParams as LayoutParams
+        params.height = height
+        params.weight = weight
+    }
+
+}
+/*
+class LoginLayout(context: Context, attributes: AttributeSet) : LinearLayout(context, attributes) {
+
+    var keyboardShowed = false
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         if (changed) {
@@ -61,4 +118,4 @@ class LoginLayout(context: Context, attributes: AttributeSet) : LinearLayout(con
         }
     }
 
-}
+}*/
